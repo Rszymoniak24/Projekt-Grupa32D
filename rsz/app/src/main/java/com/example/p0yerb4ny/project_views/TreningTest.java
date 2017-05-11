968c0436f6528bad0e7a433e7f4da266943e674e
LukaszWydrzynski <lwydrzynski@gmail.com>
2017-05-11 02:56:24 +0200

    Add files via upload

---
+++ rsz/app/src/main/java/com/example/p0yerb4ny/project_views/TreningTest.java
@@ -0,0 +1,40 @@
+package com.example.ukasz.lab3;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Łukasz on 2017-05-11.
+ */
+class Trening {
+    private int id;
+    private String nazwaTreningu;
+    private String rodzajTreningu;
+    private  String opisTreningu;
+
+    public Trening(){}
+
+    public Trening(String nazwaTreningu, String rodzajTreningu, String opisTreningu)  {
+        super();
+        this.nazwaTreningu = nazwaTreningu;
+        this.rodzajTreningu = rodzajTreningu;
+        this.opisTreningu = opisTreningu;
+
+
+    }
+    @Override
+    public String toString() {
+        return "Trening [id=" + id + ", nazwaTreningu=" + nazwaTreningu + ", rodzajTreningu=" + rodzajTreningu + opisTreningu + ", opisTreningu="+ "]";
+    }
+    public String getNazwaTreningu(){return nazwaTreningu;}
+
+    public String getRodzajTreningu() {
+        return rodzajTreningu;
+    }
+    public String getOpisTreningu() {
+        return opisTreningu;
+    }
+
+    public int getId(){return id;}
+    public void setId(int id){this.id=id;}
+}
+}
